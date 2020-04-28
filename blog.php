f4ac59bbaa967879be5427a2065a113e508bd7f3
HerMan <infabls.com@gmail.com>
2020-04-28 15:44:24 +0600

    add menu file add require section in all each files

--- blog.php
+++ blog.php
@@ -39,63 +39,7 @@
         </div>
         <!-- Preloading -->
 
-        <!-- Header 01 -->
-        <header class="header_01" id="header">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-2 col-sm-3 col-md-3">
-                        <div class="logo">
-                            <a href="index"><img src="images/logo.png" alt=""/></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-8 col-sm-7 col-md-7">
-                        <nav class="mainmenu text-center">
-                            <ul>
-                                <li class="menu-item-has-children">
-                                    <a href="#">home</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="index">Home 01</a></li>
-                                        <li><a href="index2">Home 02</a></li>
-                                        <li><a href="index3">Home 03</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="about">About</a></li>
-                                <li class="menu-item-has-children"><a href="#">Services</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="services">Service</a></li>
-                                        <li><a href="service_detail">Service Detail</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children"><a href="#">Portfolio</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="portfolio">Portfolio v1</a></li>
-                                        <li><a href="portfolio2">Portfolio v2</a></li>
-                                        <li><a href="portfolio_detail">Portfolio Detail</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children"><a href="#">Blog</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog">Blog v1</a></li>
-                                        <li><a href="blog2">Blog v2</a></li>
-                                        <li><a href="blog3">Blog v3</a></li>
-                                        <li><a href="blog_single">Blog Single</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="contact">Contact</a></li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div class="col-lg-2 col-sm-2 col-md-2">
-                        <div class="navigator text-right">
-                            <a class="search searchToggler" href="javascript:void(0);"><i class="mei-magnifying-glass"></i></a>
-                            <a href="javascript:void(0);" class="menu mobilemenu hidden-sm hidden-md hidden-lg hidden-xs"><i class="mei-menu"></i></a>
-                            <a id="open-overlay-nav" class="menu hamburger" href="javascript:void(0);"><i class="mei-menu"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- Header 01 -->
+        <?php include 'menu.php';?>
 
         <!-- Search From -->
         <div class="searchFixed popupBG">
